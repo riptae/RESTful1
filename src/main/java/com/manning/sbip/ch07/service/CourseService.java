d4047c18e37b208dd41128591681ff435c700bd5
riptae <riptaggart@naver.com>
2024-04-27 20:17:45 +0900

    service, serviceimpl

---
+++ src/main/java/com/manning/sbip/ch07/service/CourseService.java
@@ -0,0 +1,22 @@
+package com.manning.sbip.ch07.service;
+
+import com.manning.sbip.ch07.model.Course;
+
+import java.util.Optional;
+
+public interface CourseService {
+
+    Course createCourse(Course course);
+
+    Optional<Course> getCourseById(long courseId);
+
+    Iterable<Course> getCoursesByCategory(String category);
+
+    Iterable<Course> getCourses();
+
+    void updateCourse(long courseId, Course course);
+
+    void deleteCourseById(long courseId);
+
+    void deleteCourses();
+}
---
+++ src/main/java/com/manning/sbip/ch07/service/CourseServiceImpl.java
@@ -0,0 +1,58 @@
+package com.manning.sbip.ch07.service;
+
+import com.manning.sbip.ch07.model.Course;
+import com.manning.sbip.ch07.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CourseServiceImpl implements CourseService {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Override
+    public Course createCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    @Override
+    public Optional<Course> getCourseById(long courseId) {
+        return courseRepository.findById(courseId);
+    }
+
+    @Override
+    public Iterable<Course> getCoursesByCategory(String category) {
+        return courseRepository.findAllByCategory(category);
+    }
+
+    @Override
+    public Iterable<Course> getCourses() {
+        return courseRepository.findAll();
+    }
+
+    @Override
+    public void updateCourse(long courseId, Course course) {
+        courseRepository.findById(courseId).ifPresent(dbCourse -> {
+            dbCourse.setName(course.getName());
+            dbCourse.setCategory(course.getCategory());
+            dbCourse.setDescription(course.getDescription());
+            dbCourse.setRating(course.getRating());
+            courseRepository.save(dbCourse);
+        });
+    }
+
+    @Override
+    public void deleteCourses() {
+        courseRepository.deleteAll();
+    }
+
+    @Override
+    public void deleteCourseById(long courseId) {
+        courseRepository.deleteById(courseId);
+    }
+
+
+}
