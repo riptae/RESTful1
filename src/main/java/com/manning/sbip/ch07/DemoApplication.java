d99f462a1082a1a815c4cc021e1e1a3984535f01
riptae <riptaggart@naver.com>
2024-04-29 21:01:27 +0900

    Open API 문서

--- src/main/java/com/manning/sbip/ch07/DemoApplication.java
+++ src/main/java/com/manning/sbip/ch07/DemoApplication.java
@@ -1,13 +1,29 @@
 package com.manning.sbip.ch07;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${app.description}") String appDescription,
+								 @Value("${app.version}") String appVersion) {
+
+		return new OpenAPI().info(new Info().title("Course Tracker API").version(appVersion)
+				.description(appDescription).termsOfService("http://swagger.io/terms/")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
 	}
 
 }
--- src/main/java/com/manning/sbip/ch07/controller/CourseController.java
+++ src/main/java/com/manning/sbip/ch07/controller/CourseController.java
@@ -2,49 +2,67 @@ package com.manning.sbip.ch07.controller;
 
 import com.manning.sbip.ch07.model.Course;
 import com.manning.sbip.ch07.service.CourseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/courses/")
+@Tag(name = "Course Controller", description = "This REST controller provide services to manage courses in the Course Tracker application")
 public class CourseController {
 
     @Autowired
     private CourseService courseService;
 
     @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    @Operation(summary = "Provides all courses available")
     public Iterable<Course> getAllCourses() {
         return courseService.getCourses();
     }
 
     @GetMapping("{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    @Operation(summary = "Provides course detail by id")
     public Optional<Course> getCourseById(@PathVariable("id") long courseId) {
         return courseService.getCourseById(courseId);
     }
 
     @GetMapping("category/{name}")
+    @ResponseStatus(code = HttpStatus.OK)
+    @Operation(summary = "Provides course detail by category")
     public Iterable<Course> getCourseByCategory(@PathVariable("name") String category) {
         return courseService.getCoursesByCategory(category);
     }
 
     @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    @Operation(summary = "Creates a new course")
     public Course createCourse(@RequestBody Course course) {
         return courseService.createCourse(course);
     }
 
     @PutMapping("{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @Operation(summary = "Updates the course details by id")
     public Course updateCourse(@PathVariable("id") long courseId, @RequestBody Course course) {
         return courseService.updateCourse(courseId, course);
     }
 
     @DeleteMapping("{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @Operation(summary = "Deletes the course detail by id")
     void deleteCourseById(@PathVariable("id") long courseId) {
         courseService.deleteCourseById(courseId);
     }
 
     @DeleteMapping
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @Operation(summary = "Deletes all courses")
     void deleteCourses() {
         courseService.deleteCourses();
     }
