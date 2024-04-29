a7d62f04661eccc2a2de096b57d3619f7731df58
riptae <riptaggart@naver.com>
2024-04-29 17:13:02 +0900

    course작동 테스트 updateCourse 작업할것

--- src/main/java/com/manning/sbip/ch07/model/Course.java
+++ src/main/java/com/manning/sbip/ch07/model/Course.java
@@ -1,11 +1,17 @@
 package com.manning.sbip.ch07.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Course {
 
     @Id
