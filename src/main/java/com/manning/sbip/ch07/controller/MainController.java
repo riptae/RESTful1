8be807c6a5b9059bb1eaf0922d05ed3dc29b76b3
riptae <riptaggart@naver.com>
2024-05-02 20:53:12 +0900

    main컨트롤러 추가

--- src/main/java/com/manning/sbip/ch07/controller/MainController.java
+++ src/main/java/com/manning/sbip/ch07/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-    @GetMapping
+    @GetMapping("/")
     @ResponseBody
     public String main() {
         return "main";
