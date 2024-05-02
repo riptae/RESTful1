c65f20eae588c743f590023bb0ce8092ccc90177
riptae <riptaggart@naver.com>
2024-05-02 20:52:56 +0900

    main컨트롤러 추가

---
+++ src/main/java/com/manning/sbip/ch07/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.manning.sbip.ch07.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping
+    @ResponseBody
+    public String main() {
+        return "main";
+    }
+}
